09088f02356d68d098fc08d9d3bdb8191034f8b5
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2024-10-05 00:19:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -60,7 +60,7 @@ global.premium = ["918127875972"] // Premium User
 
 //bot sett
 global.typemenu = 'v1' // menu type 'v1' => 'v12'
-global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.typereply = 'v3' // reply type 'v1' => 'v4'
 global.autoblocknumber = '212' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.antidelete = false //set anti delete 
